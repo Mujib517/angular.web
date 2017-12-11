8a5dc3de201e21daccf09dbc15b33905f6c8fa6d
Mujib517 <mujib517@gmail.com>
2017-12-11 08:01:33 +0530

    Template driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from "./app.component";
@@ -15,9 +16,9 @@ import { TimePipe } from "./shared/time.pipe";
 
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpClientModule],
+    imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
     declarations: [AppComponent, HomeComponent,
-        AboutComponent, ContactComponent, ProductsComponent, ProductComponent, UsersComponent,TimePipe],
+        AboutComponent, ContactComponent, ProductsComponent, ProductComponent, UsersComponent, TimePipe],
     bootstrap: [AppComponent],
     providers: [ProductService]
 })
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ProductService } from "../shared/product.service";
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-products',
@@ -8,19 +9,28 @@ import { ProductService } from "../shared/product.service";
 export class ProductsComponent {
     products: any[];
     product: any = {};
+    frm: FormGroup;
 
-    constructor(private productSvc: ProductService) {
+    constructor(private productSvc: ProductService, private fb: FormBuilder) {
+        this.frm = fb.group({
+            brand: [],
+            model: ['X8'],
+            price: [200],
+            inStock: [true]
+        });
         this.get();
     }
 
     onSave() {
         this.product.inStock = this.product.inStock || false;
-        this.productSvc.save(this.product)
-            .subscribe(
-            () => this.get(),
-            err => this.get(),
-            () => console.log("Completed")
-            )
+
+        console.log(this.frm.value);
+        // this.productSvc.save(this.product)
+        //     .subscribe(
+        //     () => this.get(),
+        //     err => this.get(),
+        //     () => console.log("Completed")
+        //     )
     }
 
     get() {
