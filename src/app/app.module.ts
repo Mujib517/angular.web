2b41a77e365ae8aa97343cc7af831636c10dfcc3
Mujib517 <mujib517@gmail.com>
2017-12-16 07:34:14 +0530

    Barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,8 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProductsComponent } from "./product-list/product-list.component";
-import { ProductComponent } from "./product/product.component";
-import { ProductService } from "./shared/product.service";
-import { UsersComponent } from './users/users.component';
-import { TimePipe } from "./shared/time.pipe";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NewProductComponent } from './new-product/new-product.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductResolve } from "./shared/product.resolve";
-import { SpecsComponent } from './specs/specs.component';
-import { ReviewsComponent } from './reviews/reviews.component';
-import { RoutingModule } from "./shared/routing.module";
-import { SharedModule } from "./shared/shared.module";
-import { LoginComponent } from './login/login.component';
-
+import { AppComponent, HomeComponent, AboutComponent, ContactComponent, ProductsComponent, ProductComponent, UsersComponent, HeaderComponent, FooterComponent, NewProductComponent, ProductDetailComponent, SpecsComponent, ReviewsComponent, LoginComponent } from "./app.barrel";
+import { NgModule,BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule, RoutingModule, SharedModule } from "./module.barrel";
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule,
-        RoutingModule,SharedModule],
+    imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule, RoutingModule, SharedModule],
     declarations: [AppComponent, HomeComponent,
         AboutComponent, ContactComponent, ProductsComponent, ProductComponent, UsersComponent, HeaderComponent, FooterComponent, NewProductComponent, ProductDetailComponent, SpecsComponent, ReviewsComponent, LoginComponent],
     bootstrap: [AppComponent],
